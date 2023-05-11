0fdd068eeda9e7437883b0d8feeb91934883a284
hum-sc <Humberto SC>
2023-05-11 08:54:34 -0600

    Spring initialization

---
+++ src/main/java/com/osba/agenteP/AgentePApplication.java
@@ -0,0 +1,13 @@
+package com.osba.agenteP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentePApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentePApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osba/agenteP/AgentePApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osba.agenteP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentePApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
