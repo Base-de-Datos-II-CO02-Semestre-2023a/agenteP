006835ec6533e8db3dede4b6abfdca1f5f2e1d4a
dannySempai3331 <jdaniel10031@outlook.com>
2023-05-31 09:21:33 -0600

    cambios

--- src/main/java/com/osba/agenteP/domain/Ciudad.java
+++ src/main/java/com/osba/agenteP/domain/Ciudad.java
@@ -1,25 +1,16 @@
 package com.osba.agenteP.domain;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "ciudad")
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
+
 public class Ciudad {
 
-    @Id @GeneratedValue(generator = "ciudad_id_seq")
-    @SequenceGenerator(name = "ciudad_id_seq", sequenceName = "ciudad_id_seq", allocationSize = 1)
-    private String id_ciudad;
+    private Integer id_ciudad;
     private String entidad;
     private String pais;
     private String nombre;
-
 
 }
