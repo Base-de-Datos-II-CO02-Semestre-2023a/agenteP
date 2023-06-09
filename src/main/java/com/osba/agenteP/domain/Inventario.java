69c146b13f5b8b41456e2c873b9062bd4215e00a
dannySempai3331 <jdaniel10031@outlook.com>
2023-06-09 13:23:02 -0600

    otras correcciones

---
+++ src/main/java/com/osba/agenteP/domain/Inventario.java
@@ -0,0 +1,17 @@
+package com.osba.agenteP.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/*@Entity
+@Table(name = "inventario")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor*/
+public class Inventario {
+}
