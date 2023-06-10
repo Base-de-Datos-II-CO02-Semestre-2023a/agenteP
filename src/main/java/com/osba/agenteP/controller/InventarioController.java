2f9e2987f424de62bc5a115cf27265886fdeeb5b
dannySempai3331 <jdaniel10031@outlook.com>
2023-06-09 18:07:45 -0600

    endpoint /inventario/reabasteciemtoRecomendado

--- src/main/java/com/osba/agenteP/controller/InventarioController.java
+++ src/main/java/com/osba/agenteP/controller/InventarioController.java
@@ -1,5 +1,6 @@
 package com.osba.agenteP.controller;
 
+import com.osba.agenteP.domain.Inventario;
 import com.osba.agenteP.repository.InventarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -19,5 +21,14 @@ public class InventarioController {
     @GetMapping("/productosCaducar")
     public Map<String,Integer> productosACaducar(){
         return Collections.singletonMap("proximos a caducar",inventarioRepository.productosCaducar());
+    }
+    @GetMapping("/reabasteciemtoRecomendado")
+    public Map<String,Integer> reabastecimientoRecommended(){
+        return Collections.singletonMap("reabastecimiento recomendado",inventarioRepository.reabasteciemtoRecommended());
+    }
+
+    @GetMapping("/productos")
+    public List<Inventario> cosasEnInventario(){
+        return inventarioRepository.cosasEnInventario();
     }
 }
--- src/main/java/com/osba/agenteP/repository/InventarioRepository.java
+++ src/main/java/com/osba/agenteP/repository/InventarioRepository.java
@@ -4,8 +4,16 @@ import com.osba.agenteP.domain.Inventario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface InventarioRepository extends JpaRepository<Inventario,Integer> {
 
     @Query(value = "SELECT COUNT(id_articulo) FROM inventario WHERE ((caducidad - current_date) < 14) AND (caducidad > current_date)", nativeQuery = true)
     public Integer productosCaducar();
+
+    @Query(value="SELECT COUNT(id_articulo) FROM inventario WHERE (cantidad < 15)", nativeQuery = true)
+    public Integer reabasteciemtoRecommended();
+
+    @Query(value="SELECT * FROM inventario", nativeQuery = true)
+    public List<Inventario> cosasEnInventario();
 }
