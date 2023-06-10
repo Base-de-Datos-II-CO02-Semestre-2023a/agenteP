4b3ae3ba277eaa8ad2a5e0e454d36abf0ce61753
dannySempai3331 <jdaniel10031@outlook.com>
2023-06-10 01:48:51 -0600

    modelado de la tabla concepto

--- src/main/java/com/osba/agenteP/domain/Concepto.java
+++ src/main/java/com/osba/agenteP/domain/Concepto.java
@@ -1,8 +1,10 @@
 package com.osba.agenteP.domain;
 
 import com.osba.agenteP.enums.TipoMovimiento;
+import com.osba.agenteP.model.IdConcepto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ import java.util.Date;
 @Setter
 @ToString
 @NoArgsConstructor
+@IdClass(IdConcepto.class)
 public class Concepto {
 
     private Integer cantidad;
---
+++ src/main/java/com/osba/agenteP/model/IdConcepto.java
@@ -0,0 +1,9 @@
+package com.osba.agenteP.model;
+
+import com.osba.agenteP.enums.TipoMovimiento;
+
+public class IdConcepto {
+    private Integer idArticulo;
+    private Integer idMovimiento;
+    private TipoMovimiento tipo;
+}
